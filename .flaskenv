82ae455a6fa984b7e61817205e7c1c5a71b45c32
Nathaniel Johnson <nathancj224@gmail.com>
2019-10-17 03:48:52 -0400

    Basic flask server and index.html.j2 jinja template setup
    also add .flaskenv and some css

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=src/web/server.py
+FLASK_ENV=development
--- src/web/server.py
+++ src/web/server.py
@@ -1,3 +1,23 @@
-from flask import Flask
+from flask import Flask, escape, request
+from flask.templating import render_template
 
-app = Flask()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html.j2')
+
+
+@app.route('/flights/new', methods=['POST'])
+def create_flight():
+    return 'hi'
+
+
+def main():
+
+    app.run(host='localhost', port='8000', debug=True)
+
+
+if __name__ == '__main__':
+    main()
